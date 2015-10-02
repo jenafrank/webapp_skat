a92fb203e77945ab624e0da8f6375b25b69ea2cc
Frank Peuker <mail@frankpeuker.de>
2015-10-02 09:25:14 +0200

    Firebase interface: Transfer from JS Fiddle http://jsfiddle.net/xwkg6pcg/8

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -29,5 +29,112 @@ angular.module('myApp.view1', ['ngRoute'])
             $(".line3").css("opacity", "0.8");
         }, 6000);
 
+        //
+        console.log("Executed");
+        $rootScope.isOn = false;
+
+        // get bubble elements
+        var bubbleElement1 = document.getElementById("bubble1");
+        var bubbleElement2 = document.getElementById("bubble2");
+        var bubbleElement3 = document.getElementById("bubble3");
+        var x;
+
+        // Shoot first to third bubble!! yeah.
+        $timeout(function () {
+            refreshBubbleWithNumber("1");
+            bubbleElement1.classList.add('y1');
+        }, 6000);
+
+        $timeout(function () {
+            refreshBubbleWithNumber("2");
+            bubbleElement2.classList.add('y2');
+        }, 16000);
+
+        $timeout(function () {
+            refreshBubbleWithNumber("3");
+            bubbleElement3.classList.add('y3');
+        }, 26000);
+
+        // get informed when animation (one bubbling procedure) finishes and update picture
+        bubbleElement1.addEventListener("animationend", function () {
+            bubbleElement1.classList.remove('y1');
+            refreshBubbleWithNumber("1");
+            x = bubbleElement1.offsetWidth;
+            bubbleElement1.classList.add('y1');
+        }, false);
+
+        bubbleElement2.addEventListener("animationend", function () {
+            bubbleElement2.classList.remove('y2');
+            refreshBubbleWithNumber("2");
+            x = bubbleElement2.offsetWidth;
+            bubbleElement2.classList.add('y2');
+        }, false);
+
+        bubbleElement3.addEventListener("animationend", function () {
+            bubbleElement3.classList.remove('y3');
+            refreshBubbleWithNumber("3");
+            x = bubbleElement3.offsetWidth;
+            bubbleElement3.classList.add('y3');
+        }, false);
+
+        // react on selection of element
+        $rootScope.showInfo = function (bubbleNr) {
+            $rootScope.isOn = true;
+            var key = getKey(bubbleNr);
+            var re = /img[^\)"]*/;
+            var substr = re.exec(key);
+            $rootScope.bubbleInfo = $rootScope.explanations[substr[0]];
+            focusAnimation(bubbleNr);
+        };
+
+        function getKey(bubbleNr) {
+            return $(".bubble" + bubbleNr).css("background-image");
+        }
+
+        function focusAnimation(bubbleNr) {
+            var el = null;
+            if (bubbleNr === '1') {
+                el = bubbleElement1;
+            } else if (bubbleNr === '2') {
+                el = bubbleElement2;
+            } else if (bubbleNr === '3') {
+                el = bubbleElement3;
+            }
+
+            el.style.borderColor = "rgb(194, 253, 61)";
+            el.style.boxShadow = "10px 10px 5px #888888";
+            el.style.width = "200px";
+            el.style.height = "200px";
+
+            $timeout(function () {
+                $rootScope.isOn = false;
+                resetSelection();
+            }, 5000);
+        }
+
+        function resetSelection() {
+            var elements = [bubbleElement1, bubbleElement2, bubbleElement3];
+            elements.forEach(function (el) {
+                el.style.borderColor = "rgb(75,124,54)";
+                el.style.boxShadow = "none";
+                el.style.width = "150px";
+                el.style.height = "150px";
+            });
+        }
+
+        // Refresh picture and cycle through pictures
+        function refreshBubbleWithNumber(bubbleNr) {
+            $(".bubble" + bubbleNr).css("background-image", nextPicture());
+            console.log($(".bubble" + bubbleNr).css("background-image"));
+        }
+
+        $scope.picIndex = -1;
+        function nextPicture() {
+            $scope.picIndex++;
+            if ($scope.picIndex === $rootScope.pictures.length) {
+                $scope.picIndex = 0;
+            }
+            return 'url("' + $scope.pictures[$scope.picIndex] + '")';
+        }
 
     }]);
--- app/view2/view2.js
+++ app/view2/view2.js
@@ -2,13 +2,165 @@
 
 angular.module('myApp.view2', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view2', {
-    templateUrl: 'view2/view2.html',
-    controller: 'View2Ctrl'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/start', {
+            templateUrl: 'view2/view2.html',
+            controller: 'View2Ctrl'
+        });
+    }])
 
-.controller('View2Ctrl', [function() {
+    .controller('View2Ctrl', ['$scope', function ($scope) {
 
-}]);
+        $scope.isVisible = false;
+        $scope.text = "Mein Text";
+        $scope.games = [];
+
+        $scope.results = {
+            val: {},
+            teil: {},
+            ver: {},
+            gew: {},
+            ges: {},
+            verGegen: {},
+            gewGegen: {},
+            gesGegen: {},
+            ratioGegen: {},
+            ratioAllein: {},
+            ronaldFaktor: {},
+            ratioPPT: {},
+            ronaldGedeckelt: {},
+            ronaldPunkte: {},
+            turnierPunkte: {},
+            turnierRonaldPunkte: {},
+            turnierPPT: {}
+        };
+
+        $scope.arrays = {
+            ronaldPunkte: []
+        }
+
+        var x = $scope.results;
+
+        activate();
+
+        function activate() {
+            var myFirebaseRef = new Firebase("https://luminous-inferno-9676.firebaseio.com/season_22");
+            myFirebaseRef.child("/").on("value", function (snapshot) {
+                $scope.games = snapshot.val();
+                calculateResults();
+                $scope.$apply();
+            });
+        }
+
+        function calculateResults() {
+            // Akkumuliere einzelne Spiele
+            traverse($scope.games, process);
+
+            // Auswertung der akkumulierten Spieldaten
+            div($scope.results.ratioGegen, $scope.results.gewGegen, $scope.results.gesGegen);
+            div($scope.results.ratioAllein, $scope.results.gew, $scope.results.ges);
+            $scope.results.refGames = max($scope.results.teil);
+            for (var el in x.teil) {
+                x.ronaldFaktor[el] = x.refGames / x.teil[el];
+                x.ronaldGedeckelt[el] = x.ronaldFaktor[el] > 3 ? 3 : x.ronaldFaktor[el];
+                x.ronaldPunkte[el] = x.ronaldGedeckelt[el] * x.val[el];
+                x.verGegen[el] = x.gesGegen[el] - x.gewGegen[el];
+                x.ver[el] = x.ges[el] - x.gew[el];
+                x.turnierPunkte[el] = x.val[el] + 50 * (x.gew[el] - x.ver[el]) + 40 * x.gewGegen[el];
+                x.turnierRonaldPunkte[el] = x.turnierPunkte[el] * x.ronaldGedeckelt[el];
+                x.turnierPPT[el] = x.turnierPunkte[el] / x.teil[el];
+            }
+            div(x.ratioPPT, x.val, x.teil);
+            $scope.arrays.ronaldPunkte = json2array(x.ronaldPunkte);
+        }
+
+        function acc(key, attr, value) {
+            if (key[attr]) {
+                key[attr] += value;
+            } else {
+                key[attr] = value;
+            }
+        }
+
+        function div(key, pathZ, pathN) {
+            for (var attr in pathZ) {
+                if (attr != 'E') {
+                    var z = pathZ[attr];
+                    var n = pathN[attr];
+                    if (n < 1) {
+                        n = 0;
+                    }
+                    key[attr] = z / n;
+                }
+            }
+        }
+
+        function max(path) {
+            var max = 0;
+            for (var key in path) {
+                if (path[key] > max) {
+                    max = path[key];
+                }
+            }
+            return max;
+        }
+
+        function json2array(collection) {
+            var array = [];
+            for (var attr in collection) {
+                array.push({
+                    name: attr,
+                    value: collection[attr]
+                });
+            }
+            return array;
+        }
+
+        function traverse(o, func) {
+            for (var i in o) {
+                func.apply(this, [i, o[i]]);
+                if (o[i] !== null && typeof (o[i]) == "object") {
+                    //going on step down in the object tree!!
+                    traverse(o[i], func);
+                }
+            }
+        }
+
+        function process(key, value) {
+            var substr = key.substring(0, 4);
+            if (substr === 'game') {
+
+                var threeplayers = value.activeThree.split(" ");
+                var points = parseInt(value.points);
+
+                // Punkte zusammenzählen
+                acc($scope.results.val, value.declarer, points);
+
+                // Teilgenommene Spiele
+                acc($scope.results.teil, threeplayers[0], 1);
+                acc($scope.results.teil, threeplayers[1], 1);
+                acc($scope.results.teil, threeplayers[2], 1);
+
+                // Spiele insgesamt in Skatrunde
+                acc($scope.results, "nrGames", 1);
+
+                // Gewonnen und gespielt als Alleinspieler
+                if (value.points > 0) {
+                    acc($scope.results.gew, value.declarer, 1);
+                }
+                acc($scope.results.ges, value.declarer, 1);
+
+                // Gewonnen und gespielt als Gegenspieler
+                threeplayers.forEach(function (el) {
+                    if (el != value.declarer) {
+                        acc($scope.results.gesGegen, el, 1);
+                        if (points < 0) {
+                            acc($scope.results.gewGegen, el, 1);
+                        }
+                    }
+                });
+
+            }
+        }
+
+    }]);
